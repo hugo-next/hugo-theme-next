6865ec72c7373cd6b0920006fd948fab299e67cd
凡梦星尘 <elkan1788@139.com>
2022-10-22 12:55:11 +0800

    :ambulance: Enable the home page register tool button click.

--- assets/js/next-boot.js
+++ assets/js/next-boot.js
@@ -10,9 +10,7 @@ NexT.boot.registerEvents = function() {
 
   NexT.utils.registerScrollPercent();
   // NexT.utils.registerCanIUseTag();
-  if (NexT.CONFIG.page.isPage) {
-    NexT.utils.registerToolButtons();
-  }
+  NexT.utils.registerToolButtons();
 
   // Mobile top menu bar.
   document.querySelector('.site-nav-toggle .toggle').addEventListener('click', event => {
